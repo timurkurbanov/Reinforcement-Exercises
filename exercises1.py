871c8d656dcbbdf18f0c60fbc16186b330121445
TIMUR <1tkurbanov@gmail.com>
2019-05-08 15:40:19 -0400

    1step

---
+++ exercises1.py
@@ -0,0 +1,26 @@
+#Create a list for each item below that contains the given information:
+fav_colours = ['brown', 'yellow', 'red']
+age = ['five', 'one', 'ten']
+coin = ['heads', 'tails', 'heads', 'heads']
+article= ['Ricky Martin', 'Betman', 'Sher'] 
+
+#Create a dictionary for each item below that contains the given information:
+
+three_words = {
+    'Ironman': 'strong',
+    'Betman': 'respectful',
+    'Spiderman': 'thinkful' 
+}
+fav_movie = {
+    'Back to the Future': '1985',
+    'Titanic': '2000'
+}
+three_cities = {
+    'Toronto': 3,
+    'Oakville': 1,
+    'Bloomfield': 1'
+}
+name_siblings = {
+    'Danile': 7,
+    'Sofia': 5
+}
